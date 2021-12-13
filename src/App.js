11ad0ca6668d8427a2e1344937c78b4f302fc6da
github username <cmyerskeitt@gmail.com>
2021-12-13 00:53:58 -0500

    Add Display and first card  to Home page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import React from 'react'
 import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Home from './components/pages/Home';
 
 function App() {
   return (
+    <div>
       <Router>
         <Navbar/>
+         <Home/>
         {/* <Switch>
-          <Route path='/'/>
+          <Route exact path='/'>
+            <Home/>
+          </Route>
+          <Route exact path='/about'>
+            <About/>
+          </Route>
+          <Route exact path='/contact'>
+            <Contact />
+          </Route>
+          <Route exact path='/pricing'>
+            <Pricing/>
+          </Route>
         </Switch> */}
       </Router>
+    </div>
   );
 }
 
---
+++ src/components/pages/Home.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import NSA_Badge from '../images/nsa_member_badge.png'
+import NSA_Trained_Badge from '../images/nsa-trained-badge.png'
+import Logo from '../images/DiamondNotary.png'
+import './Home.css'
+
+function Home() {
+    return (
+        <div className="b-example-divider container-fluid">
+            <h1 className='welcome display-1 text-success'> Welcome to Diamond Notary DMV, LLC.</h1>
+            <div class="card">
+            <img src={Logo} class="card-img-top" alt="Diamond DMV Logo"/>   
+            <div class="card-body">
+            <h2 className='info card-text '> Commision a professional Notary and Loan Signing agent with over 10 years  of experience notarizing accounting, real estate and property management documents. My reliability and attention to detail will make you a happy returning customer. 
+            <br/> 
+            <br/>
+            <label className="info1 card-title">Call Me Today to Schedule Your Signing!
+            <br/>
+            (240)-424-9577
+            </label>
+            </h2>
+            <h2 className='hours btn btn-success'> Hours of Operation <br></br>
+            Monday-Friday: 3pm - 9pm <br></br>
+            Saturday: 9 am - 9pm <br></br>
+            Sunday: 9 am - 9 pm </h2>
+            {/* <img src={NSA_Badge} className='rounded float-start"' alt='NSA Badge'/>
+            <img src={Logo} className='rounded mx-auto d-block' alt='Logo'/>
+            <img src={NSA_Trained_Badge} className='rounded float-end' alt='NSA Trained Badge'/> */}
+        
+            <br></br>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
