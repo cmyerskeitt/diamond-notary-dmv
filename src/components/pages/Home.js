5b18082174566f399d6815a1e1cce0fcb610ab87
github username <cmyerskeitt@gmail.com>
2022-01-30 20:08:56 -0500

    finalize styling for home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -9,34 +9,31 @@ function Home() {
         <div className="b-example-divider">
             <h1 className='welcome display-1'> Welcome to Diamond Notary DMV, LLC.</h1>
             <section class="card">
-            <img src={Logo} class="card-img-top" alt="Diamond DMV Logo"/>   
+            <img src={Logo} class="card-img-top" alt="Diamond DMV Logo"/> 
+            <h2 className="info"> Looking for a notary? </h2>   
                 <section class="card-body"> 
-                    <h2 className="info"> Looking for a notary? </h2> 
                     <h3 className='card-text'>Commision a professional Notary/Loan Signing agent with over 10 years  of experience related to:</h3>
                         <ul>
                             <li> accounting</li>
                             <li> real estate</li>
                             <li> property management</li>
                         </ul>
+                        <img src={NSA_Badge} className='rounded float-start' alt='NSA Badge'/>
+                        <img src={NSA_Trained_Badge} className='rounded float-end' alt='NSA Trained Badge'/>
+                        <br></br>
+                        <h2> My reliability and attention to detail will make you a happy returning customer. </h2>
                 </section>
-            </section>    
-            {/* <h2 className='info card-text '> Commision a professional Notary and Loan Signing agent with over 10 years  of experience notarizing accounting, real estate and property management documents. My reliability and attention to detail will make you a happy returning customer. 
-            <br/> 
-            <br/>
-            <label className="info1 card-title">Call Me Today to Schedule Your Signing!
-            <br/>
-            (240)-424-9577
-            </label>
-            </h2>
-            <h2 className='hours btn btn-success'> Hours of Operation <br></br>
-            Monday-Friday: 3pm - 9pm <br></br>
-            Saturday: 9 am - 9pm <br></br>
-            Sunday: 9 am - 9 pm </h2>
-            <img src={NSA_Badge} className='rounded float-start"' alt='NSA Badge'/>
-            <img src={NSA_Trained_Badge} className='rounded float-end' alt='NSA Trained Badge'/>
-        
-            <br></br> */}
-           
+            </section>   
+            <section className='info1'>
+                <h2><em>Call Me Today to Schedule Your Signing!</em><br></br>(240)-424-9577</h2>
+                <br></br>
+                <h2>
+                    Hours of Operation <br></br>
+                    Monday-Friday: 3pm - 9pm <br></br>
+                    Saturday: 9 am - 9pm <br></br>
+                    Sunday: 9 am - 9 pm 
+                </h2> 
+            </section>
         </div>
     )
 }
