daf128b2b7f229b05832c63de03c9deca5d8c98b
github username <cmyerskeitt@gmail.com>
2022-03-16 20:23:07 -0400

    changed black to #212529 to match nav

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -15,9 +15,10 @@ function Home() {
                 {/* <h2 className="info"> Looking for a notary? </h2>   */}
                 <h2 className='welcome display-1'> Welcome to Diamond Notary DMV, LLC.</h2> 
                     <section class="card-body"> 
+                    <br></br>
                         <h3 className='card-text'> Commision a professional Notary agent with over 10 years  of experience in:</h3>
-                            <ul>
-                                <li> accounting</li>
+                            <ul >
+                                <li > accounting</li>
                                 <li> real estate</li>
                                 <li> property management</li>
                             </ul>
@@ -25,14 +26,15 @@ function Home() {
                             <img src={NSA_Trained_Badge} className='rounded float-end' alt='NSA Trained Badge'/>
                             <br></br>
                             <br></br>
-                            <br></br>
-                            <h3 className='card-text'> Call Me Today to Schedule Your Signing!  <br></br> My reliability and attention to detail will make you a happy returning customer. </h3>
+                            <br></br>  
+                            <h3 className='card-text'> <em> Call (240) 424-9577 </em><br></br>Schedule your signing with Notary Agent Deyonna Roberts today! <br></br> My reliability and attention to detail will make you a happy returning customer. </h3>
                             
                     </section>
                 </section>   
                 <footer className='info1'>
                     <br></br>
                     <h2>
+                        
                         Hours of Operation <br></br>
                         Monday-Friday: 3pm - 9pm <br></br>
                         Saturday: 9 am - 9pm <br></br>
