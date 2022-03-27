ff8ab27efa89a61136e9a767d5c5d42ba202e867
github username <cmyerskeitt@gmail.com>
2022-03-27 15:44:35 -0400

    remove logo in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,11 +7,7 @@ function Navbar() {
     return (
      
         <nav class="navbar navbar-expand-lg bg-dark">
-            {/* <nav class="navbar navbar-expand-lg navbar-dark bg-dark"></nav> */}
             <div class="container-fluid">
-                <Link to='/' class="navbar-brand" >
-                    <img src={Logo} width="100" height="100" class="d-inline-block align-text-top" alt='Diamond Notary Logo'/>       
-                </Link>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -8,28 +8,20 @@ function Home() {
     return (
         <div class="container-fluid">
             <div className="b-example-divider">
-                {/* <h1 className='welcome display-1'> Welcome to Diamond Notary DMV, LLC.</h1> */}
                 <h1 className='info'> Looking for a Notary?</h1>
                 <section class="card">
-                <img src={Logo} class="card-img-top" alt="Diamond DMV Logo"/> 
-                {/* <h2 className="info"> Looking for a notary? </h2>   */}
+                <img src={Logo} class="card-img-top" alt="Diamond DMV Logo"/>
                 <h2 className='welcome display-1'> Welcome to Diamond Notary DMV, LLC.</h2> 
-                    <section class="card-body"> 
-                    <br></br>
-                        <h3 className='card-text'> Commision a professional Notary agent with over 10 years  of experience in:</h3>
+                <img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.squarespace-cdn.com%2Fcontent%2Fv1%2F60209f40dacf4b64ff2dcd1c%2F1613179570008-V0O0E15KZ54JYJPUUZNF%2Fke17ZwdGBToddI8pDm48kEt_v2EX9GtAJgLtqLXx-3J7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0gmXcXvEVFTLbYX9CdVcGe6CRawwjMbRFT4hbu0QnHz9xuVto2nJwMxrL2rUquKQ0A%2Fimage-asset.jpeg%3Fformat%3D2500w&f=1&nofb=1' className='capital' alt="Capital BUilding"></img>
+                 <h3> Commision a professional Notary agent with over 10 years  of experience in:</h3>
                             <ul >
                                 <li > accounting</li>
                                 <li> real estate</li>
                                 <li> property management</li>
                             </ul>
-                            <img src={NSA_Badge} className='rounded float-start' alt='NSA Badge'/>
-                            <img src={NSA_Trained_Badge} className='rounded float-end' alt='NSA Trained Badge'/>
-                            <br></br>
-                            <br></br>
-                            <br></br>  
-                            <h3 className='card-text'> <em> Call (240) 424-9577 </em><br></br>Schedule your signing with Notary Agent Deyonna Roberts today! <br></br> My reliability and attention to detail will make you a happy returning customer. </h3>
-                            
-                    </section>
+                            {/* <img src={NSA_Badge} className='rounded float-start' alt='NSA Badge'/>
+                            <img src={NSA_Trained_Badge} className='rounded float-end' alt='NSA Trained Badge'/> */}
+                    <h3 className=''> <em> Call (240) 424-9577 </em><br></br>Schedule your signing with Notary Agent Deyonna Roberts today! <br></br> My reliability and attention to detail will make you a happy returning customer. </h3>
                 </section>   
                 <footer className='info1'>
                     <br></br>
